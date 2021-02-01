9f9e41c3283cb3c0b51062a14dd738a1d6bed077
Agustina Coppe <agustina.coppe@endava.com>
2021-02-01 14:29:37 -0300

    Add game logic

--- src/Game.js
+++ src/Game.js
@@ -2,14 +2,31 @@ import React, { Component } from "react";
 import Card from "./Card";
 
 export default class Game extends Component {
-  componentDidMount() {
-    console.log(this.props.gameData);
+  createRandomSelection(n) {
+    let indexNumbers = [];
+    while (indexNumbers.length !== n * 2) {
+      let randomNum = Math.floor(Math.random() * n);
+      if (
+        indexNumbers.indexOf(randomNum) === -1 ||
+        indexNumbers.indexOf(randomNum) === indexNumbers.lastIndexOf(randomNum)
+      ) {
+        indexNumbers.push(randomNum);
+      }
+    }
+    return indexNumbers;
+  }
+  createGame() {
+    const randomData = [];
+    const amount = this.props.gameData.length;
+    const selection = this.createRandomSelection(amount);
+    selection.forEach((index) => randomData.push(this.props.gameData[index]));
+    return randomData;
   }
   render() {
-    const { gameData } = this.props;
+    const data = this.createGame();
     return (
       <div className="Game">
-        {gameData.map((element) => (
+        {data.map((element) => (
           <Card img={element.img} />
         ))}
       </div>
