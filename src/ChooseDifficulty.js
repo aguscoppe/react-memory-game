4d80a48c0a6a3afc96b919ca38a40caabb9709cb
Agustina Coppe <agustina.coppe@endava.com>
2021-02-01 13:30:21 -0300

    Add images

--- src/ChooseDifficulty.js
+++ src/ChooseDifficulty.js
@@ -4,7 +4,7 @@ import { withRouter } from "react-router-dom";
 class ChooseDifficulty extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: "medium" };
+    this.state = { value: "easy" };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -23,9 +23,7 @@ class ChooseDifficulty extends Component {
           <span>Choose difficulty: </span>
           <select value={this.state.value} onChange={this.handleChange}>
             <option value="easy">Easy</option>
-            <option defaultValue value="medium">
-              Medium
-            </option>
+            <option value="medium">Medium</option>
             <option value="hard">Hard</option>
           </select>
           <div>
--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
+import Card from "./Card";
 
 export default class Game extends Component {
   componentDidMount() {
     console.log(this.props.gameData);
   }
   render() {
+    const { gameData } = this.props;
     return (
-      <div>
-        <h1>Game</h1>
+      <div className="Game">
+        {gameData.map((element) => (
+          <Card img={element.img} />
+        ))}
       </div>
     );
   }
