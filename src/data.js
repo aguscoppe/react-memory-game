aaf1261c09b4ced6d815236868c1da20a070b633
Agustina Coppe <agustina.coppe@endava.com>
2021-02-03 19:05:25 -0300

    Add images

--- src/data.js
+++ src/data.js
@@ -10,22 +10,48 @@ import walrus from "./img/009-walrus.png";
 import squid from "./img/010-squid.png";
 /* 11 - 26 */
 import flamingo from "./img/011-flamingo.png";
+import squirrel from "./img/012-squirrel.png";
+import crab from "./img/013-crab.png";
+import toucan from "./img/014-toucan.png";
+import deer from "./img/015-deer.png";
+import fish from "./img/016-fish.png";
+import snail from "./img/017-snail.png";
+import horse from "./img/018-horse.png";
+import jellyfish from "./img/019-jellyfish.png";
+import rabbit from "./img/020-rabbit.png";
+import seahorse from "./img/021-seahorse.png";
+import pig from "./img/022-pig.png";
+import octopus from "./img/023-octopus.png";
+import whale from "./img/024-whale.png";
+import duck from "./img/025-duck.png";
+import hedgehog from "./img/026-hedgehog.png";
 /* 27 - 50 */
 import monkey from "./img/027-monkey.png";
+import hermitCrab from "./img/028-hermitcrab.png";
+import kangaroo from "./img/029-kangaroo.png";
+import lobster from "./img/030-lobster.png";
+import turtle from "./img/031-turtle.png";
+import lion from "./img/032-lion.png";
+import cat from "./img/033-cat.png";
+import panda from "./img/034-panda.png";
+import owl from "./img/035-owl.png";
+import koala from "./img/036-koala.png";
+import dog from "./img/037-dog.png";
+import sheep from "./img/038-sheep.png";
+import seal from "./img/039-seal.png";
+import meerkat from "./img/040-meerkat.png";
+import llama from "./img/041-llama.png";
+import shark from "./img/042-shark.png";
+import bison from "./img/043-bison.png";
+import hippopotamus from "./img/044-hippopotamus.png";
+import penguin from "./img/045-penguin.png";
+import chameleon from "./img/046-chameleon.png";
+import camel from "./img/047-camel.png";
+import ostrich from "./img/048-ostrich.png";
+import bee from "./img/049-bee.png";
+import swan from "./img/050-swan.png";
 
 const gameData = {
-  /*easy: [
-    { id: "01", img: elephant, animal: "elephant" },
-    { id: "02", img: hen, animal: "hen" },
-    { id: "03", img: giraffe, animal: "giraffe" },
-    { id: "04", img: frog, animal: "frog" },
-    { id: "05", img: dolphin, animal: "dolphin" },
-    { id: "06", img: parrot, animal: "parrot" },
-    { id: "07", img: kiwi, animal: "kiwi" },
-    { id: "08", img: snake, animal: "snake" },
-    { id: "09", img: walrus, animal: "walrus" },
-    { id: "10", img: squid, animal: "squid" },
-  ],*/
   easy: [
     elephant,
     hen,
@@ -38,8 +64,50 @@ const gameData = {
     walrus,
     squid,
   ],
-  medium: [flamingo],
-  hard: [monkey],
+  medium: [
+    flamingo,
+    squirrel,
+    crab,
+    toucan,
+    deer,
+    fish,
+    snail,
+    horse,
+    jellyfish,
+    rabbit,
+    seahorse,
+    pig,
+    octopus,
+    whale,
+    duck,
+    hedgehog,
+  ],
+  hard: [
+    monkey,
+    hermitCrab,
+    kangaroo,
+    lobster,
+    turtle,
+    lion,
+    cat,
+    panda,
+    owl,
+    koala,
+    dog,
+    sheep,
+    seal,
+    meerkat,
+    llama,
+    shark,
+    bison,
+    hippopotamus,
+    penguin,
+    chameleon,
+    camel,
+    ostrich,
+    bee,
+    swan,
+  ],
 };
 
 export default gameData;
